48ab92ffedbc66287a26beb06c43f8893caaf43a
Ian Young <ian@iay.org.uk>
2014-05-31 15:21:47 +0100

    Some initial collection rendering logic.

--- src/main/java/uk/org/iay/mdq/server/MetadataService.java
+++ src/main/java/uk/org/iay/mdq/server/MetadataService.java
@@ -16,17 +16,21 @@
 
 package uk.org.iay.mdq.server;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.annotation.Resource;
-import javax.inject.Named;
+import javax.annotation.Nonnull;
 
 import net.shibboleth.metadata.Item;
+import net.shibboleth.metadata.ItemSerializer;
 import net.shibboleth.metadata.pipeline.Pipeline;
 import net.shibboleth.metadata.pipeline.PipelineProcessingException;
 import net.shibboleth.utilities.java.support.component.AbstractIdentifiableInitializableComponent;
 import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.component.ComponentSupport;
+import net.shibboleth.utilities.java.support.logic.Constraint;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,23 +48,106 @@ public class MetadataService<T> extends AbstractIdentifiableInitializableCompone
     /**
      * The pipeline to execute to acquire metadata.
      */
-    @Resource
-    @Named("metadataSource.SAML")
-    private Pipeline<T> metadataSourcePipeline;
+    private Pipeline<T> sourcePipeline;
+    
+    /**
+     * The pipeline to execute to render metadata for publication.
+     */
+    private Pipeline<T> renderPipeline;
+
+    /**
+     * The serializer to use to convert the rendered metadata into
+     * an octet stream.
+     * 
+     * This should really be part of the render pipeline itself but
+     * we need a new stage to do that.
+     */
+    private ItemSerializer<T> serializer;
 
     /**
      * The metadata we have acquired from the source pipeline.
      */
     private Collection<Item<T>> itemCollection;
+    
+    public void setSourcePipeline(@Nonnull final Pipeline<T> pipeline) {
+        ComponentSupport.ifDestroyedThrowDestroyedComponentException(this);
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+
+        sourcePipeline = Constraint.isNotNull(pipeline, "source pipeline may not be null");
+    }
+    
+    public void setRenderPipeline(@Nonnull final Pipeline<T> pipeline) {
+        ComponentSupport.ifDestroyedThrowDestroyedComponentException(this);
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+
+        renderPipeline = Constraint.isNotNull(pipeline, "render pipeline may not be null");
+    }
+    
+    public void setSerializer(@Nonnull final ItemSerializer<T> itemSerializer) {
+        ComponentSupport.ifDestroyedThrowDestroyedComponentException(this);
+        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
+
+        serializer = Constraint.isNotNull(itemSerializer, "serializer may not be null");
+    }
+    
+    private Collection<Item<T>> cloneItemCollection(@Nonnull final Collection<Item<T>> collection) {
+        final Collection<Item<T>> newItems = new ArrayList<>();
+        for (Item<T> item : collection) {
+            newItems.add(item.copy());
+        }
+        return newItems;
+    }
+    
+    private byte[] renderCollection(@Nonnull final Collection<Item<T>> collection) {
+        final Collection<Item<T>> items = cloneItemCollection(collection);
+        try {
+            log.debug("rendering collection of {} elements", items.size());
+            renderPipeline.execute(items);
+            log.debug("items rendered, resulting collection has {} elements", items.size());
+            try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+                for (Item<T> item : items) {
+                    serializer.serialize(item, os);
+                }
+                return os.toByteArray();
+            }
+        } catch (IOException e) {
+            log.debug("problem with output stream: " + e.getMessage());
+            return null;
+        } catch (PipelineProcessingException e) {
+            log.debug("problem with render pipeline: " + e.getMessage());
+            return null;
+        }
+    }
+    
+    public byte[] getAll() {
+        return renderCollection(itemCollection);
+    }
+    
+    public byte[] get(@Nonnull final String identifier) {
+        return null;
+    }
 
     /** {@inheritDoc} */
     @Override
     protected void doInitialize() throws ComponentInitializationException {
         super.doInitialize();
+
+        if (sourcePipeline == null) {
+            throw new ComponentInitializationException("source pipeline must be supplied");
+        }
+        
+        if (renderPipeline == null) {
+            throw new ComponentInitializationException("render pipeline must be supplied");
+        }
+        
+        if (serializer == null) {
+            throw new ComponentInitializationException("serializer must be supplied");
+        }
+
         try {
             final Collection<Item<T>> newItemCollection = new ArrayList<>();
             log.debug("executing source pipeline");
-            metadataSourcePipeline.execute(newItemCollection);
+            sourcePipeline.execute(newItemCollection);
             log.debug("source pipeline executed; {} results", newItemCollection.size());
             itemCollection = newItemCollection;
         } catch (PipelineProcessingException e) {
@@ -72,7 +159,9 @@ public class MetadataService<T> extends AbstractIdentifiableInitializableCompone
     @Override
     protected void doDestroy() {
         itemCollection = null;
-        metadataSourcePipeline = null;
+        sourcePipeline = null;
+        renderPipeline = null;
+        serializer = null;
         super.doDestroy();
     }
 
